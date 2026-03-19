c3971b828f4eaf7d56c4e5cb88b8a85e833a777d
Shubhankmandloi <shubhank2mandloi@gmail.com>
2026-03-19 23:40:33 +0530

    done

---
+++ Servlet/FirstServletProgram/src/main/java/in/co/myservlet/MyServlet.java
@@ -0,0 +1,20 @@
+package in.co.myservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet
+{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.print("I am in doget method");
+		System.out.println("I am in doget method");
+	}
+}
